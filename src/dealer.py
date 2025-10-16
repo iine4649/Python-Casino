6ef282710131a35b919bf1c1d9265bf21f55d0ea
siddiqik <skomou27@nmhschool.org>
2025-10-16 08:09:04 -0400

    defined a card, working on dealer too

---
+++ src/dealer.py
@@ -0,0 +1,16 @@
+class Dealer:
+    def __init__(self):
+
+    def reset_hand(self, card):
+    # clear dealers hand when the game ends
+
+    def add_card(self):
+    #  adds cards to the dealers hand
+
+    def get_total(self):
+    # calculates the totoal from the dealers hand
+
+    def better_to_hit(self):
+    # outlines the situation where it is better for teh dealer to hit
+
+    def show_hand(self,):
