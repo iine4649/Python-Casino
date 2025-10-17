0affd6b94cf8b3756548b60808a8993a996cab53
siddiqik <skomou27@nmhschool.org>
2025-10-16 23:23:21 -0400

    dealer data

--- src/dealer.py
+++ src/dealer.py
@@ -1,16 +1,29 @@
+from card import Card
+from typing import List
+from game import BlackjackGame
+
 class Dealer:
     def __init__(self):
+        self.hand: List[Card] = []
 
-    def reset_hand(self, card):
-    # clear dealers hand when the game ends
+    def reset_hand(self):
+        self.hand = []
 
-    def add_card(self):
-    #  adds cards to the dealers hand
+    def add_card(self, card: Card):
+        self.hand.append(card)
 
     def get_total(self):
-    # calculates the totoal from the dealers hand
+        total = sum(card.get_value() for card in self.hand)
+        aces = sum(1 for card in self.hand if card.cards == "Ace")
+        while total > 21 and aces:
+            total -= 10
+            aces -= 1
+        return total
 
-    def better_to_hit(self):
-    # outlines the situation where it is better for teh dealer to hit
+    def dealer_turn(self, deck):
+        while self.get_total() < 17:
+            card = deck.draw_card()
+            self.add_card(card)
 
-    def show_hand(self,):
+    def show_hand(self):
+        return [str(card) for card in self.hand]
