90a327bd83edd5a9528bf2cb4970068c62af7ff9
idkwhattonamemyself115 <khu27@nmhschool.org>
2025-10-17 09:17:51 -0400

    gut

--- src/game.py
+++ src/game.py
@@ -1,5 +1,69 @@
 import random
+<<<<<<< Updated upstream
 
+=======
+class BlackjackGame:
+    def __init__(self):
+        self.user = []
+        self.bot = []
+        self.game_over = False
+        self.player_stayed = False
+        self.round = 1
+        self.cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+
+    def card_value(self, card):
+        if card.isdigit():    
+            return int(card)
+        elif card in ['J', 'Q', 'K']:
+            return 10
+        elif card == 'A' and self.calculate_total(self.user)<=10:
+            return 11
+        elif card == 'A' and self.calculate_total(self.user)>10:
+            return 1
+
+    
+    def draw_card(self, who):
+        card = random.choice(self.cards)
+        value = self.card_value(card)
+        print(f"{who.capitalize()} drew a {card}")
+
+        if who == 'user':
+            self.user.append(card)
+        else:
+            self.bot.append(card)
+        return card, value
+    
+    def calculate_total(self, hand):
+        total = sum(self.card_value(card) for card in hand)
+        return total
+    
+    def hit_player(self):
+        if not self.game_over:
+            self.draw_card('user')
+            if self.calculate_total(self.user) > 21:
+                self.game_over = True
+            return self.user
+        return None
+    def get_game_result(self):
+        user_total = self.calculate_total(self.user)
+        bot_total = self.calculate_total(self.bot)
+        
+        if user_total ==21:
+            return 'win'
+        elif bot_total ==21:
+            return 'lose'
+        elif user_total > 21:
+            return 'lose'
+        elif bot_total > 21:
+            return 'win'
+        elif user_total > bot_total:
+            return 'win'
+        elif user_total < bot_total:
+            return 'lose'
+        else:
+            return 'tie'
+>>>>>>> Stashed changes
 
 '''
 bot=[]
