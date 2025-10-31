012ece5213610eb3148ae6331e52c440388224c1
idkwhattonamemyself115 <khu27@nmhschool.org>
2025-10-31 16:16:28 -0400

    card.py debug+blackjack structure

--- src/card.py
+++ src/card.py
@@ -1,12 +1,9 @@
-import copy
 import random
-import game
 
 # standard card combinations
 cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-         "Jack", "Queen ", "King", "Ace"]
-card_color = ["Red", "Black"]
-card_suits = ["Club", "Diamond", "Heart", "Spades"]
+         "Jack", "Queen", "King", "Ace"]
+card_suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 suit_colors = {
     "Hearts": "Red",
     "Diamonds": "Red",
@@ -14,39 +11,47 @@ suit_colors = {
     "Spades": "Black"
 }
 
-# card dimensions *make sure that it is in sync with UI*
+# card dimensions
 width = 300
 height = 450
 
-class Card:
-    def __init__(self):(self, cards, card_types, card_color):
-        self.cards = cards
-        self.card_types = card_type
-        self.card_color = card_color
-    def get_value(self):
-        if self.cards = ["King", "Queen", "Jack"]:
-            return 10
-        elif self.cards == "Ace"
-            return.random(1, 11)
-        else:
-            return int(self.card_rank)
-    def __str__(self):
-        return f"{self.cards} or {self.type}"
 
-# deck generation
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.color = suit_colors[suit]
+
+    def get_value(self):
+        if self.rank in ["King", "Queen", "Jack"]:
+            return 10
+        elif self.rank == "Ace":
+            return random.choice([1, 11])
+        else:
+            return int(self.rank)
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit} ({self.color})"
+
+
 def card_deck():
+    """Generate a standard 52-card deck."""
     deck = []
     for suit in card_suits:
-        for card in cards:
-            card = Card(card, suit)
+        for rank in cards:
+            deck.append(Card(rank, suit))
     return deck
 
-def shuffle_cards(deck_count = 5):
-    reg_deck = card_deck()
-    full_deck = copy.deepcopy(reg_deck * full_deck)
-    random.shuffle(full_deck)
-    return full_deck
+
+def shuffle_cards(deck_count=1):
+    """Create and shuffle a deck or multiple decks."""
+    deck = card_deck() * deck_count
+    random.shuffle(deck)
+    return deck
 
 
-
-
+# Debug test
+if __name__ == "__main__":
+    test_deck = shuffle_cards(2)
+    for card in test_deck[:10]:
+        print(card)
